2840c3dbd28aeaec7e308f2becffd7cd8610c35a
Sayed_Ali <99877388+Sayed2710@users.noreply.github.com>
2022-04-18 22:14:47 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,6 +1,6 @@
 
 var Num = localStorage.getItem('product');
-$.getJSON('../js/Products.json', function (data) {
+$.getJSON('../js/Products.json', function (data){
     document.title = `${data[Num - 1].name}`;
     var pro = '';
 
